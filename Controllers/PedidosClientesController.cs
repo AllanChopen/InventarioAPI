ebf5600c697745083a1320fb85efaaaffc52ed68
Allan Chopen <allanchopen@gmail.com>
2026-03-21 17:13:47 -0600

    Fix: add missing using directives in PedidosClientesController.cs

--- Controllers/PedidosClientesController.cs
+++ Controllers/PedidosClientesController.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using InventarioAPI.Data;
 using InventarioAPI.DTOs;
 using InventarioAPI.Models;
